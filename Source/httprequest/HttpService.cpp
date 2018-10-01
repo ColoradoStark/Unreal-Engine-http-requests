24ded7d4d7512a178732288a7d853eb290a69656
Colorado Stark <coloradostark@yahoo.com>
2018-10-01 21:53:22 +0300

    Initial commit of finished project

---
+++ Source/httprequest/HttpService.cpp
@@ -0,0 +1,260 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HttpService.h"
+
+AHttpService::AHttpService() { PrimaryActorTick.bCanEverTick = false; }
+void AHttpService::BeginPlay() {
+	Super::BeginPlay();
+	Http = &FHttpModule::Get();
+
+	// You can uncomment these sections to test.
+
+	//////////////////////////////////////////////////////////
+	// LOGIN
+
+	//FRequest_Login LoginCredentials;
+	//LoginCredentials.email = TEXT("asdf@asdf.com");
+	//LoginCredentials.password = TEXT("asdfasdf");
+	//Login(LoginCredentials);
+
+	//////////////////////////////////////////////////////////
+	// CREATE
+	
+	//FRequest_CreateNewPlayer PlayerCredentials;
+	//PlayerCredentials.playername = TEXT("Navajo");
+	//PlayerCredentials.playerclass = TEXT("wizard");
+	//CreateNewPlayer(PlayerCredentials);
+
+	//////////////////////////////////////////////////////////
+	// READ
+
+	GetPlayerById(2);
+
+	//////////////////////////////////////////////////////////
+	// UPDATE
+
+	/*
+	FRequest_UpdatePlayer NewPlayerData;
+	NewPlayerData.id = 4;
+	NewPlayerData.playername = TEXT("Sequoyah");
+	NewPlayerData.playerclass = TEXT("wizard");
+	NewPlayerData.weapon = TEXT("staff");
+	NewPlayerData.weaponelement = TEXT("none"); 
+	NewPlayerData.playerlevel = 101;
+	NewPlayerData.weaponlevel = 0;
+	NewPlayerData.hp = 20;
+	NewPlayerData.mana = 20;
+	NewPlayerData.gold = 100;
+	NewPlayerData.strength = 9;
+	NewPlayerData.dexterity = 9;
+	NewPlayerData.intelligence = 9;
+	NewPlayerData.constitution = 9;
+
+	UpdatePlayer(NewPlayerData);
+	*/
+
+	//////////////////////////////////////////////////////////
+	// DELETE
+
+	//DeletePlayerById(9);
+}
+
+
+
+
+/**************************************************************************************************************************/
+
+
+
+TSharedRef<IHttpRequest> AHttpService::RequestWithRoute(FString Subroute) {
+	TSharedRef<IHttpRequest> Request = Http->CreateRequest();
+	Request->SetURL(ApiBaseUrl + Subroute);
+	SetRequestHeaders(Request);
+	return Request;
+}
+
+void AHttpService::SetRequestHeaders(TSharedRef<IHttpRequest>& Request) {
+	Request->SetHeader(TEXT("User-Agent"), TEXT("X-UnrealEngine-Agent"));
+	Request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+	Request->SetHeader(TEXT("Accepts"), TEXT("application/json"));
+}
+
+TSharedRef<IHttpRequest> AHttpService::GetRequest(FString Subroute) {
+	TSharedRef<IHttpRequest> Request = RequestWithRoute(Subroute);
+	Request->SetVerb("GET");
+	return Request;
+}
+
+TSharedRef<IHttpRequest> AHttpService::PostRequest(FString Subroute, FString ContentJsonString) {
+	TSharedRef<IHttpRequest> Request = RequestWithRoute(Subroute);
+	Request->SetVerb("POST");
+	Request->SetContentAsString(ContentJsonString);
+	return Request;
+}
+
+TSharedRef<IHttpRequest> AHttpService::DeleteRequest(FString Subroute) {
+	TSharedRef<IHttpRequest> Request = RequestWithRoute(Subroute);
+	Request->SetVerb("DELETE");
+	return Request;
+}
+
+TSharedRef<IHttpRequest> AHttpService::PutRequest(FString Subroute, FString ContentJsonString) {
+	TSharedRef<IHttpRequest> Request = RequestWithRoute(Subroute);
+	Request->SetVerb("PUT");
+	Request->SetContentAsString(ContentJsonString);
+	return Request;
+}
+
+void AHttpService::Send(TSharedRef<IHttpRequest>& Request) {
+	Request->ProcessRequest();
+}
+
+bool AHttpService::ResponseIsValid(FHttpResponsePtr Response, bool bWasSuccessful) {
+	if (!bWasSuccessful || !Response.IsValid()) return false;
+	if (EHttpResponseCodes::IsOk(Response->GetResponseCode())) return true;
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Http Response returned error code: %d"), Response->GetResponseCode());
+		return false;
+	}
+}
+
+void AHttpService::SetAuthorizationHash(FString Hash, TSharedRef<IHttpRequest>& Request) {
+	Request->SetHeader(AuthorizationHeader, Hash);
+}
+
+
+
+/**************************************************************************************************************************/
+
+
+
+template <typename StructType>
+void AHttpService::GetJsonStringFromStruct(StructType FilledStruct, FString& StringOutput) {
+	FJsonObjectConverter::UStructToJsonObjectString(StructType::StaticStruct(), &FilledStruct, StringOutput, 0, 0);
+}
+
+template <typename StructType>
+void AHttpService::GetStructFromJsonString(FHttpResponsePtr Response, StructType& StructOutput) {
+	StructType StructData;
+	FString JsonString = Response->GetContentAsString();
+	FJsonObjectConverter::JsonObjectStringToUStruct<StructType>(JsonString, &StructOutput, 0, 0);
+}
+
+
+
+/**************************************************************************************************************************/
+
+
+
+void AHttpService::Login(FRequest_Login LoginCredentials) {
+	FString ContentJsonString;
+	GetJsonStringFromStruct<FRequest_Login>(LoginCredentials, ContentJsonString);
+
+	TSharedRef<IHttpRequest> Request = PostRequest("user/logi", ContentJsonString);
+	Request->OnProcessRequestComplete().BindUObject(this, &AHttpService::LoginResponse);
+	Send(Request);
+}
+
+void AHttpService::LoginResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful) {
+	if (!ResponseIsValid(Response, bWasSuccessful)) return;
+
+	FResponse_Login LoginResponse;
+	GetStructFromJsonString<FResponse_Login>(Response, LoginResponse);
+
+	Response_Login_Cache.id = LoginResponse.id;
+	Response_Login_Cache.name = LoginResponse.name;
+	Response_Login_Cache.hash = LoginResponse.hash;
+	Response_Login_Cache.isChanged = LoginResponse.isChanged;
+
+	UE_LOG(LogTemp, Warning, TEXT("Id is: %d"), LoginResponse.id);
+	UE_LOG(LogTemp, Warning, TEXT("Name is: %s"), *LoginResponse.name);
+}
+
+
+
+void AHttpService::CreateNewPlayer(FRequest_CreateNewPlayer PlayerCredentials) {
+	FString ContentJsonString;
+	GetJsonStringFromStruct<FRequest_CreateNewPlayer>(PlayerCredentials, ContentJsonString);
+
+	TSharedRef<IHttpRequest> Request = PostRequest("api/createnewplayer", ContentJsonString);
+	Request->OnProcessRequestComplete().BindUObject(this, &AHttpService::CreateNewPlayerResponse);
+	Send(Request);
+}
+
+void AHttpService::CreateNewPlayerResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful) {
+	if (!ResponseIsValid(Response, bWasSuccessful)) return;
+
+	FResponse_CreateNewPlayer CreateNewPlayerResponse;
+	GetStructFromJsonString<FResponse_CreateNewPlayer>(Response, CreateNewPlayerResponse);
+
+	UE_LOG(LogTemp, Warning, TEXT("Result of operation to create player was: %s"), *CreateNewPlayerResponse.message);
+	UE_LOG(LogTemp, Warning, TEXT("Your Newly Created Player ID is: %d"), CreateNewPlayerResponse.id);
+}
+
+void AHttpService::GetPlayerById(int PlayerId) {
+
+	FString subroute("api/getplayerbyid/");
+	FString id = FString::FromInt(PlayerId);
+	subroute.Append(id);
+
+	//UE_LOG(LogTemp, Warning, TEXT("The subroute path is: %s"), *subroute);
+	
+	
+
+	TSharedRef<IHttpRequest> Request = GetRequest(subroute);
+	Request->OnProcessRequestComplete().BindUObject(this, &AHttpService::GetPlayerByIdResponse);
+	Send(Request);
+}
+
+void AHttpService::GetPlayerByIdResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful) {
+	if (!ResponseIsValid(Response, bWasSuccessful)) return;
+
+	FResponse_GetPlayerById GetPlayerByIdResponse;
+	GetStructFromJsonString<FResponse_GetPlayerById>(Response, GetPlayerByIdResponse);
+
+	UE_LOG(LogTemp, Warning, TEXT("You retrieved player ID number: %d"), GetPlayerByIdResponse.id);
+	UE_LOG(LogTemp, Warning, TEXT("The name of this player is : %s"), *GetPlayerByIdResponse.playername);
+}
+
+void AHttpService::UpdatePlayer(FRequest_UpdatePlayer NewPlayerData) {
+	FString ContentJsonString;
+	GetJsonStringFromStruct<FRequest_UpdatePlayer>(NewPlayerData, ContentJsonString);
+
+	TSharedRef<IHttpRequest> Request = PutRequest("api/updateplayer", ContentJsonString);
+	Request->OnProcessRequestComplete().BindUObject(this, &AHttpService::UpdatePlayerResponse);
+	Send(Request);
+}
+
+void AHttpService::UpdatePlayerResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful) {
+	if (!ResponseIsValid(Response, bWasSuccessful)) return;
+
+	FResponse_UpdatePlayer UpdatePlayerResponse;
+	GetStructFromJsonString<FResponse_UpdatePlayer>(Response, UpdatePlayerResponse);
+
+	UE_LOG(LogTemp, Warning, TEXT("Result of operation to update player was: %s"), *UpdatePlayerResponse.message);
+	UE_LOG(LogTemp, Warning, TEXT("You Updated Player ID: %d"), UpdatePlayerResponse.id);
+}
+
+
+void AHttpService::DeletePlayerById(int PlayerId) {
+
+	FString subroute("api/deleteplayerbyid/");
+	FString id = FString::FromInt(PlayerId);
+	subroute.Append(id);
+
+	//UE_LOG(LogTemp, Warning, TEXT("The subroute path is: %s"), *subroute);
+
+	TSharedRef<IHttpRequest> Request = DeleteRequest(subroute);
+	Request->OnProcessRequestComplete().BindUObject(this, &AHttpService::DeletePlayerByIdResponse);
+	Send(Request);
+}
+
+void AHttpService::DeletePlayerByIdResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful) {
+	if (!ResponseIsValid(Response, bWasSuccessful)) return;
+
+	FResponse_DeletePlayerById DeletePlayerByIdResponse;
+	GetStructFromJsonString<FResponse_DeletePlayerById>(Response, DeletePlayerByIdResponse);
+
+	UE_LOG(LogTemp, Warning, TEXT("The result of the delete operation was: %d"), DeletePlayerByIdResponse.id);
+	UE_LOG(LogTemp, Warning, TEXT("The number of the deleted player is : %s"), *DeletePlayerByIdResponse.message);
+}
---
+++ Source/httprequest/HttpService.h
@@ -0,0 +1,183 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Runtime/Online/HTTP/Public/Http.h"
+#include "Json.h"
+#include "Runtime/JsonUtilities/Public/JsonUtilities.h"
+#include "HttpService.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FRequest_Login {
+	GENERATED_BODY()
+		UPROPERTY(EditAnywhere, BlueprintReadWrite) FString email;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FString password;
+
+	FRequest_Login() {}
+};
+
+USTRUCT(BlueprintType)
+struct FResponse_Login {
+	GENERATED_BODY()
+		UPROPERTY(EditAnywhere, BlueprintReadWrite) int id;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FString name;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FString hash;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) bool isChanged;
+
+	FResponse_Login() {}
+};
+
+USTRUCT(BlueprintType)
+struct FResponse_GetPlayerById {
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int id = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FString playername;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FString playerclass;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FString weapon;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FString weaponelement;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int playerlevel = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int weaponlevel = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int hp = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int mana = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int gold = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int strength = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int dexterity = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int intelligence = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int constitution = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int reg_date; //TODO This is the wrong type in PHP Change in PHP to time since epoch
+
+
+	//FResponse_GetPlayerById() {} TODO: REMOVE ALL OF THESE CONSTRUCTORS
+};
+
+USTRUCT(BlueprintType)
+struct FRequest_CreateNewPlayer {
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FString playername;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FString playerclass;
+
+	FRequest_CreateNewPlayer() {}
+};
+
+USTRUCT(BlueprintType)
+struct FResponse_CreateNewPlayer {
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int id;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FString message;
+
+	FResponse_CreateNewPlayer() {}
+};
+
+USTRUCT(BlueprintType)
+struct FResponse_DeletePlayerById {
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int id;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FString message;
+
+	FResponse_DeletePlayerById() {}
+};
+
+USTRUCT(BlueprintType)
+struct FRequest_UpdatePlayer {
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int id;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FString playername;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FString playerclass;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FString weapon;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FString weaponelement;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int playerlevel;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int weaponlevel;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int hp;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int mana;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int gold;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int strength;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int dexterity;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int intelligence;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int constitution;
+
+	FRequest_UpdatePlayer() {}
+};
+
+USTRUCT(BlueprintType)
+struct FResponse_UpdatePlayer {
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int id;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FString message;
+
+	FResponse_UpdatePlayer() {}
+};
+
+
+USTRUCT(BlueprintType)
+struct FPlovdivUniversity {
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int students;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FString teachers;
+};
+
+
+
+
+
+UCLASS()
+class HTTPREQUEST_API AHttpService : public AActor
+{
+	GENERATED_BODY()
+
+//private:
+protected:
+	FHttpModule* Http;
+	//FString ApiBaseUrl = "http://localhost:8080/";
+	FString ApiBaseUrl = "http://52.214.193.147/";
+
+	FString AuthorizationHeader = TEXT("Authorization");
+	void SetAuthorizationHash(FString Hash, TSharedRef<IHttpRequest>& Request);
+
+	TSharedRef<IHttpRequest> RequestWithRoute(FString Subroute);
+	void SetRequestHeaders(TSharedRef<IHttpRequest>& Request);
+
+	TSharedRef<IHttpRequest> GetRequest(FString Subroute);
+	TSharedRef<IHttpRequest> PostRequest(FString Subroute, FString ContentJsonString);
+	TSharedRef<IHttpRequest> DeleteRequest(FString Subroute);
+	TSharedRef<IHttpRequest> PutRequest(FString Subroute, FString ContentJsonString);
+	void Send(TSharedRef<IHttpRequest>& Request);
+
+	bool ResponseIsValid(FHttpResponsePtr Response, bool bWasSuccessful);
+
+	template <typename StructType>
+	void GetJsonStringFromStruct(StructType FilledStruct, FString& StringOutput);
+	template <typename StructType>
+	void GetStructFromJsonString(FHttpResponsePtr Response, StructType& StructOutput);
+	
+public:	
+	// Sets default values for this actor's properties
+	AHttpService();
+	virtual void BeginPlay() override;
+
+	UPROPERTY(BlueprintReadWrite)
+	FResponse_Login Response_Login_Cache;
+
+	UFUNCTION(BlueprintCallable)
+	void Login(FRequest_Login LoginCredentials);
+	void LoginResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+
+	UFUNCTION(BlueprintCallable)
+	void CreateNewPlayer(FRequest_CreateNewPlayer PlayerCredentials);   
+	void CreateNewPlayerResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+
+	UFUNCTION(BlueprintCallable)
+	void GetPlayerById(int PlayerID);
+	void GetPlayerByIdResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);  
+
+	UFUNCTION(BlueprintCallable)
+	void UpdatePlayer(FRequest_UpdatePlayer NewPlayerData);
+	void UpdatePlayerResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+
+	UFUNCTION(BlueprintCallable)
+	void DeletePlayerById(int PlayerID);
+	void DeletePlayerByIdResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+	
+};
