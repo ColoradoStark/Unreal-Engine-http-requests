c83d85d0a71caf16073e0938bcff2cb18ef6681c
Colorado Stark <coloradostark@yahoo.com>
2021-09-28 20:31:08 +0300

    Create 05_MakeTheFunctionCall.cpp

---
+++ excercise-code/05_MakeTheFunctionCall.cpp
@@ -0,0 +1,19 @@
+AHttpService::AHttpService() { PrimaryActorTick.bCanEverTick = false; }
+void AHttpService::BeginPlay() {
+	Super::BeginPlay();
+	Http = &FHttpModule::Get();
+
+	//FRequest_Create PlayerCredentials;
+	//PlayerCredentials.name = TEXT("Bilbo");
+	//PlayerCredentials.ip = TEXT("200.100.50.25");
+	//PlayerCredentials.hp = 100;
+	//PlayerCredentials.mana = 100;
+
+	//Create(PlayerCredentials);
+
+	FRequest_GetPlayerInfo Player;
+	Player.id = 2;
+
+	GetPlayerInfo(Player);
+
+}
