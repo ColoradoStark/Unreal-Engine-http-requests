c72c8924c119fa667faee73f844bbdb01f34e79e
Colorado Stark <coloradostark@yahoo.com>
2021-09-28 16:33:45 +0300

    Create 03_MakeFunctionDeclarations.h

---
+++ excercise-code/03_MakeFunctionDeclarations.h
@@ -0,0 +1,3 @@
+	UFUNCTION(BlueprintCallable)
+	void GetPlayerInfo(FRequest_Create GetPlayerInfoCredentials);
+	void GetPlayerInfoResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
